62e05bd099eecbde1e3fb571d3b8beaf4678f680
yinquan <yinquan@tencent.com>
2023-07-05 00:37:20 +0800

    通用头文件

---
+++ CommonHeader.h
@@ -0,0 +1,15 @@
+//
+//  CommonHeader.h
+//  MacTerminal
+//
+//  Created by yinquan on 2023/6/15.
+//
+
+#ifndef CommonHeader_h
+#define CommonHeader_h
+#include <string>
+#include <map>
+#include <vector>
+
+
+#endif /* CommonHeader_h */
