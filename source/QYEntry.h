5c0d391de311f1f504da2e5a21bad8e9155b6bcd
yinquan <yinquan@tencent.com>
2023-08-16 21:14:18 +0800

    加入v8依赖

---
+++ source/QYEntry.h
@@ -0,0 +1,17 @@
+//
+//  QYEntry.h
+//  QYView
+//
+//  Created by 全寅 on 2023/8/16.
+//
+
+#ifndef qyentry_h
+#define qyentry_h
+
+//void initializeEVO(int argc, const char * argv[]);
+//void initialize(int argc, const char * argv[]);
+void *parseHTML(char *html);
+
+
+#endif
+
