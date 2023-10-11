5aab6383b6527b61ca1ee227721aba99f1681768
yinquan <>
2023-10-12 01:12:52 +0800

    test

--- source/QYBaseWidget.cpp
+++ source/QYBaseWidget.cpp
@@ -3,3 +3,12 @@ void QYBaseWidget::addChild(QYBaseWidget *child) {
     mChildWidgets.push_back(child);
     child->mParent = this;
 }
+
+
+void QYBaseWidget::setProperty(std::string key, QYPropertyValue *value) {
+    mPropertyValueMap.insert(std::pair(key, value));
+}
+
+QYPropertyValue* QYBaseWidget::getProperty(std::string key) {
+    return mPropertyValueMap[key];
+}
