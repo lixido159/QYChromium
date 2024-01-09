7186f47b2002496612b19002223d629fc9e8d893
yinquan <>
2024-01-10 00:45:34 +0800

    feature:创建ifNode类，准备支持qy:if属性

---
+++ source/IfFor/QYIfDomNode.hpp
@@ -0,0 +1,13 @@
+//
+//  QYIfDomNode.hpp
+//  QYView
+//
+//  Created by yinquan on 2024/1/10.
+//
+
+#ifndef QYIfDomNode_hpp
+#define QYIfDomNode_hpp
+
+#include <stdio.h>
+
+#endif /* QYIfDomNode_hpp */
