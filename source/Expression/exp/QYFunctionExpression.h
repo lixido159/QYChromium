45c98604d0cb5ce4c2dd96b6d4af3ea5c6e8c94f
yinquan <>
2024-04-16 21:13:37 +0800

    feature:升级Xcode 15.3，解决编译问题

--- source/Expression/exp/QYFunctionExpression.h
+++ source/Expression/exp/QYFunctionExpression.h
@@ -10,12 +10,13 @@
 
 #include <stdio.h>
 #include "QYExpression.h"
+#include <vector>
 class QYFunctionExpression : public QYExpression{
 public:
     QYFunctionExpression(std::shared_ptr<QYExpression> val, std::vector<std::shared_ptr<QYExpression> > args);
     virtual std::shared_ptr<QYExpResult> getExpResult(std::shared_ptr<QYExpressionContext> expContext) override;
 private:
     std::shared_ptr<QYExpression> mVal;
-    std::vector<std::shared_ptr<QYExpression> > mArgs;
+    std::vector<std::shared_ptr<QYExpression>> mArgs;
 };
 #endif /* QYFunctionExpression_hpp */
